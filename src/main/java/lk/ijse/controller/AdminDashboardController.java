1dc61518cbfc7108bd4a5ec4e09432ba89f980bf
nisaljayasekara <j.nmjayasekara@gmail.com>
2024-03-14 14:04:53 +0530

    navigate fixed

--- src/main/java/lk/ijse/controller/AdminDashboardController.java
+++ src/main/java/lk/ijse/controller/AdminDashboardController.java
@@ -1,4 +1,71 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AdminDashboardController {
+
+    @FXML
+    private AnchorPane root;
+    @FXML
+    void btnBooksOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane;
+        anchorPane = FXMLLoader.<AnchorPane>load(getClass().getResource("/view/admin/AdminBooksPage.fxml"));
+        this.root.getChildren().clear();
+        this.root.getChildren().add(anchorPane);
+    }
+
+    @FXML
+    void btnBranchesOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane;
+        anchorPane = FXMLLoader.<AnchorPane>load(getClass().getResource("/view/admin/BranchesPage.fxml"));
+        this.root.getChildren().clear();
+        this.root.getChildren().add(anchorPane);
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane;
+        anchorPane = FXMLLoader.<AnchorPane>load(getClass().getResource("/view/AdminBooks.fxml"));
+        this.root.getChildren().clear();
+        this.root.getChildren().add(anchorPane);
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/AdminSignInPage.fxml"));
+        Scene scene = new Scene(rootNode);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Admin SignIn");
+        stage.show();
+
+        Stage dashboard= (Stage) root.getScene().getWindow();
+        dashboard.close();
+    }
+
+    @FXML
+    void btnRecordsOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane;
+        anchorPane = FXMLLoader.<AnchorPane>load(getClass().getResource("/view/admin/AdminRecords.fxml"));
+        this.root.getChildren().clear();
+        this.root.getChildren().add(anchorPane);
+    }
+
+    @FXML
+    void btnUsersOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane;
+        anchorPane = FXMLLoader.<AnchorPane>load(getClass().getResource("/view/admin/AdminUsersPage.fxml"));
+        this.root.getChildren().clear();
+        this.root.getChildren().add(anchorPane);
+    }
+
 }
--- src/main/java/lk/ijse/controller/AdminSignInController.java
+++ src/main/java/lk/ijse/controller/AdminSignInController.java
@@ -1,4 +1,26 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminSignInController {
+
+
+    @FXML
+    private AnchorPane adminSignInPage;
+
+    @FXML
+    private TextField txtAdminName;
+
+    @FXML
+    private TextField txtAdminPassword;
+
+    @FXML
+    void btnAdminSignOnAction(ActionEvent event) {
+
+
+    }
+
 }
